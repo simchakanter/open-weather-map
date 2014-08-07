5c4f5c233c8f35baa92f662fbea03a1af6636657
Simcha Kanter <simchakanter@gmail.com>
2014-08-07 17:11:48 -0500

    Finish routing lesson 2.1.2

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,19 +1,41 @@
 var owmApp = angular.module('OWMApp', ['ngRoute']);
 
+owmApp.value('owmCities', ['New York', 'Dallas', 'Chicago']);
+
 owmApp.config(function($routeProvider){
-  $routeProvider.when('/', {
+  $routeProvider
+  .when('/', {
     templateUrl : './home.html',
     controller : 'HomeCtrl'
-  }).when('/cities/:city', {
+  })
+  .when('/cities/:city', {
     templateUrl: './city.html',
-    controller: 'CityCtrl'
+    controller: 'CityCtrl',
+    resolve: {
+      city: function(owmCities, $route, $location) {
+        var city = $route.current.params.city;
+        if (owmCities.indexOf(city) == -1) {
+          $location.path('/error');
+          return;
+        }
+        return city;
+      }
+    }
+  })
+  .when('/error', {
+    template : '<p>Error Page Not Found</p>'
+  })
+  .otherwise({
+    redirectTo: '/error'
   });
+  // I left out a run method here, as I couldn't figure out the lesson.
+  // It appears that its not needed.
 });
 
 owmApp.controller('HomeCtrl', function($scope) {
     //empty for now
 });
 
-owmApp.controller('CityCtrl', function($scope, $routeParams) {
-  $scope.city = $routeParams.city;
+owmApp.controller('CityCtrl', function($scope, city) {
+  $scope.city = city;
 });
